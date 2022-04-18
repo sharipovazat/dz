a41ad6450d2df66470c9494363eb5e7d0aa4e99b
sharipovazat <rekenchuk@mail.ru>
2022-04-18 16:17:18 +0500

    Задача 3 решена

--- dz_3/task_3.py
+++ dz_3/task_3.py
@@ -1,15 +1,9 @@
-names = ('Иван', 'Мария', 'Петр', 'Илья')
-def thesaurus(names):
+def thesaurus():
+    names = ('Иван', 'Мария', 'Петр', 'Илья')
     names_dict = {}
     for name in names:
-        first_letter_name = name[0]
-        print(names_dict)
-        if names_dict.get(first_letter_name):
-            print('Уже есть такая буква')
-            names_dict[first_letter_name] = name
-        else:
-            print('пока нет такой буквы', first_letter_name)
-            names_dict[first_letter_name] = name
+        names_dict[name[0]] = names_dict.get(name[0], []) + [name]
+        #print(names_dict)
     return names_dict
 
-print(thesaurus(names))
+print(thesaurus())
