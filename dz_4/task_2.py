77f1f793ed55dff9973818ebc892da86bc9e9b57
sharipovazat <rekenchuk@mail.ru>
2022-04-21 17:15:37 +0500

    2 задача 4 блока

--- dz_4/task_2.py
+++ dz_4/task_2.py
@@ -1,21 +1,12 @@
-import requests
-from requests import get, utils
+from requests import get
 from decimal import Decimal
 
-link = 'http://www.cbr.ru/scripts/XML_daily.asp'
-print(requests.get(link).text)
-
 def currency_rates(char_code):
-    link = 'http://www.cbr.ru/scripts/XML_daily.asp'
-    responce = get(link).text
-    #encodings = utils.get_encoding_from_headers(responce.headers)
-    #content = responce.content.decode(encoding=encodings)
-
+    responce = get('http://www.cbr.ru/scripts/XML_daily.asp').text
     currency_dict = {}
-    for n in responce.split('</Value>'): # находим код валюты и её значение
-        i = n.split('</CharCode')[0][-3:]
-        currency_dict[i] = n[-7:] if i.isalpha() else print()   # создаем список с валютой и её стоимостью
-
+    for line in responce.split('</Value>'):
+        parts_of_line = line.split('</CharCode')[0][-3:]
+        currency_dict[parts_of_line] = line[-7:] if parts_of_line.isalpha() else print()
     char_code = char_code.upper()
     if currency_dict.setdefault(char_code) == None:
         print('Неверный код валюты')
@@ -24,4 +15,3 @@ def currency_rates(char_code):
         print(f'{char_code} = {price} руб.')
 
 currency_rates(input('Введите код валюты: '))
-#print(currency_rates())
