4f9df62ea62595f4bca88489fba28f82173b818e
sharipovazat <rekenchuk@mail.ru>
2022-06-12 20:06:03 +0500

    Уроки

--- dz_2/task_1.py
+++ dz_2/task_1.py
@@ -1,11 +1,11 @@
-print(15*3, type(15*3))
-print(15/3, type(15/3))
-print(15//3, type(15//3))
-print(15**3, type(15**3))
+print(15*3, type(15*3))                     # 45 <class 'int'>
+print(15/3, type(15/3))                     # 5.0 <class 'float'>
+print(15//3, type(15//3))                   # 5 <class 'int'>
+print(15**3, type(15**3))                   # 3375 <class 'int'>
 
 
 a_list = [15*3, 15/3, 15//3, 15**3]
 my_map = list(map(type, a_list))
-print(my_map)
+print(my_map)                               # [<class 'int'>, <class 'float'>, <class 'int'>, <class 'int'>]
 
-print(*map(type, a_list))
+print(*map(type, a_list))                   # <class 'int'> <class 'float'> <class 'int'> <class 'int'>
