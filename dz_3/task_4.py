b4e1be36aed60fe8bcbb224b75c79baa3f6a69fb
sharipovazat <rekenchuk@mail.ru>
2022-06-12 19:18:26 +0500

    Уроки

--- dz_3/task_4.py
+++ dz_3/task_4.py
@@ -16,12 +16,6 @@
 #     }
 # }
 
-a_dict = {
-     "А": {"П": ["Петр Алексеев"]},
-     "И": {"И": ["Илья Иванов"]},
-     "С": {"И": ["Иван Сергеев", "Инна Серова"],
-           "А": ["Анна Савельева"]}
-}
 
 def thesaurus(*args):
     #namelist = list(map(str, args))                                                                        # ['Иван Сергеев', 'Инна Серова', 'Петр Алексеев', 'Илья Иванов', 'Анна Савельева']
@@ -29,16 +23,16 @@ def thesaurus(*args):
     dict_surnames = {}
     for i in args:                                                                                       # Иван Сергеев
         surname = i.split(' ')[-1].title()                                                               # Сергеев
-        first_letter_surname = surname[0]                                                                   # C
+        first_letter_surname = surname[0]                                                                # C
         dict_surnames[first_letter_surname] = dict_surnames.get(first_letter_surname, []) + [i]          # {'С': ['Иван Сергеев', 'Инна Серова', 'Анна Савельева'], 'А': ['Петр Алексеев'], 'И': ['Илья Иванов']}
-    print(dict_surnames)
+    print(dict_surnames)                                                                                    # {'С': ['Иван Сергеев', 'Инна Серова', 'Анна Савельева'], 'А': ['Петр Алексеев'], 'И': ['Илья Иванов']}
     for value in dict_surnames.values():                                                                    # ['Иван Сергеев', 'Инна Серова', 'Анна Савельева']     ['Петр Алексеев']    ['Илья Иванов']
         dict_names = {}
-        for name_surname in value:                                                                          # Иван Сергеев
-            surname = name_surname.split(' ')[-1].title()                                                   # Сергеев
-            first_letter_surname = surname[0]                                                               # C
-            first_letter_name = name_surname[0]                                                             # И
-            dict_names[first_letter_name] = dict_names.get(first_letter_name, []) + [name_surname]          # {'И' : [Иван Сергеев]}
+        for i in value:                                                                          # Иван Сергеев
+            surname = i.split(' ')[-1].title()                                                   # Сергеев
+            first_letter_surname = surname[0]                                                    # C
+            first_letter_name = i[0]                                                             # И
+            dict_names[first_letter_name] = dict_names.get(first_letter_name, []) + [i]          # {'И' : [Иван Сергеев]}
             #dict_names[first_letter_name] = sorted(dict_names.get(first_letter_name, []) + [name_surname])
             dict_main[first_letter_surname] = dict_main.get(first_letter_surname, dict_names)               # {'C' : {'И': ['Иван Сергеев']}
     return print(dict(sorted(dict_main.items())))                                                           # {'А': {'П': ['Петр Алексеев']}, 'И': {'И': ['Илья Иванов']}, 'С': {'И': ['Иван Сергеев', 'Инна Серова'], 'А': ['Анна Савельева']}}
--- dz_3/task_5.py
+++ dz_3/task_5.py
@@ -1,42 +1,107 @@
-from random import choice, randrange
+# Реализовать функцию get_jokes(), возвращающую n шуток,
+# сформированных из трех случайных слов, взятых из трёх списков (по одному из каждого):
+# nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+# adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+# adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+from random import choice, randrange, randint
+
+# шутки повторяются, количество предложений 1
+def get_jokes_1():
+    nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+    adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+    adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+    print(f'{choice(nouns).title()} {choice(adverbs)} {choice(adjectives)}')            # Лес завтра яркий
+
+get_jokes_1()
 
 
-def get_jokes(x):
-    """
-    x = int(input('Введите количество шуток: '))
-    если нужно чтобы пользователь вводил сам число нужных шуток
-    """
-
-    while x > 0:
-        print(choice(nouns), choice(adverbs), choice(adjectives))
-        x -= 1
-
-nouns = ['автомобиль', 'лес', 'огонь', 'город', 'дом']
-adverbs = ['сегодня', 'вчера', 'завтра', 'позавчера', 'ночью']
-adjectives = ['веселый', 'яркий', 'зеленый', 'утопичный', 'мягкий']
-
-get_jokes(2)
-
-print()
-print('НЕПОВТОРЯЮЩИЕСЯ ШУТКИ:')
-print()
 
 
-def new_get_jokes(x):
+# шутки повторяются, количество предложений выбирает пользователь
+def get_jokes_x(x):                                                                       # Дом вчера мягкий
+
+    nouns = ['автомобиль', 'лес', 'огонь', 'город', 'дом']
+    adverbs = ['сегодня', 'вчера', 'завтра', 'позавчера', 'ночью']
+    adjectives = ['веселый', 'яркий', 'зеленый', 'утопичный', 'мягкий']
+
+    x = int(input('Введите количество шуток: '))                                        # Город сегодня яркий
+    while x > 0:                                                                        # Лес завтра яркий
+        print(choice(nouns).capitalize(), choice(adverbs), choice(adjectives))          # Огонь ночью веселый
+        x -= 1                                                                          # Лес вчера веселый
+
+get_jokes_x(2)
+
+
+
+
+
+
+# шутки НЕ повторяются, метод randrange() количество предложений задано в аргументе функции
+def get_jokes_randrange(x):
+
+    nouns_1 = ['автомобиль', 'лес', 'огонь', 'город', 'дом']
+    adverbs_1 = ['сегодня', 'вчера', 'завтра', 'позавчера', 'ночью']
+    adjectives_1 = ['веселый', 'яркий', 'зеленый', 'утопичный', 'мягкий']
+
     while x > 0:
         random_ind_nouns = randrange(len(nouns_1))
         random_ind_adverbs = randrange(len(adverbs_1))
         random_ind_adjectives = randrange(len(adjectives_1))
+
         print(nouns_1[random_ind_nouns], adverbs_1[random_ind_adverbs], adjectives_1[random_ind_adjectives])
         nouns_1.remove(nouns_1[random_ind_nouns])
         adverbs_1.remove(adverbs_1[random_ind_adverbs])
         adjectives_1.remove(adjectives_1[random_ind_adjectives])
         x -= 1
 
-nouns_1 = ['автомобиль', 'лес', 'огонь', 'город', 'дом']
-adverbs_1 = ['сегодня', 'вчера', 'завтра', 'позавчера', 'ночью']
-adjectives_1 = ['веселый', 'яркий', 'зеленый', 'утопичный', 'мягкий']
+get_jokes_randrange(3)
 
-new_get_jokes(5)
 
+
+
+
+# шутки НЕ повторяются, метод choice(), количество шуток пользователь выбирает сам
+def get_jokes_choice():
+    question = int(input('Сколько шуток сгенерировать? '))
+    nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+    adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+    adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+    i = question
+    while i > 0:
+        choiced_nouns = choice(nouns)
+        choiced_adverbs = choice(adverbs)
+        choiced_adjectives = choice(adjectives)
+        print(f'{choiced_nouns.title()} {choiced_adverbs} {choiced_adjectives}')            # Лес завтра яркий
+        nouns.remove(choiced_nouns)
+        adverbs.remove(choiced_adverbs)
+        adjectives.remove(choiced_adjectives)
+        i -= 1
+
+get_jokes_choice()
+
+
+
+
+# шутки НЕ повторяются, метод randint(), количество шуток пользователь выбирает сам
+def get_jokes_randint():
+    question = int(input('Сколько шуток сгенерировать? '))
+    nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+    adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+    adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+    i = question
+    while i > 0:
+        randint_nouns = randint(0, len(nouns)-1)
+        randint_adverbs = randint(0, len(adverbs)-1)
+        randint_adjectives = randint(0, len(adjectives)-1)
+        print(f'{nouns[randint_nouns]} {adverbs[randint_adverbs]} {adjectives[randint_adjectives]}')            # лес позавчера мягкий
+        nouns.pop(randint_nouns)                                                                                # дом ночью яркий
+        adverbs.pop(randint_adverbs)                                                                            # город сегодня веселый
+        adjectives.pop(randint_adjectives)
+        i -= 1
+
+get_jokes_randint()
 
